cfe74c7f4a15850555bff0f379a01f3046eeb037
yangjp <99852590+yangjunpeng-leader@users.noreply.github.com>
2023-06-08 09:08:24 +0800

    提交了宿管管理系统

---
+++ src/main/java/com/example/Eight/common/Result.java
@@ -0,0 +1,45 @@
+package com.example.Eight.common;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 12572
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result<T> {
+    private String code;
+    private String msg;
+    private T data;
+
+    public Result(T data) {
+        this.data = data;
+    }
+
+    public static Result success() {
+        Result result = new Result<>();
+        result.setCode("0");
+        result.setMsg("成功");
+        return result;
+    }
+
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<>(data);
+        result.setCode("0");
+        result.setMsg("成功");
+        result.setData(data);
+        return result;
+    }
+
+    public static Result error(String code, String msg) {
+        Result result = new Result<>();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+
+}
