5040af009ebcdc8357b4447a6ea016167ec93493
yangjp <1257282045@qq.com>
2023-06-08 09:04:14 +0800

    提交了实体类

---
+++ src/main/java/com/example/Eight/EndOfTermCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndOfTermCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndOfTermCodeApplication.class, args);
+    }
+
+}
