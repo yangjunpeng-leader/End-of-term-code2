7c9a250f3ec3e77d3c8065be6f05837e08cc735c
yangjp <1257282045@qq.com>
2023-06-10 20:27:07 +0800

    完善了部分功能

---
+++ src/main/java/com/example/Eight/mapper/AdminMapper.java
@@ -0,0 +1,11 @@
+package com.example.Eight.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.Eight.entity.Admin;
+
+
+/**
+ * @author 12572
+ */
+public interface AdminMapper extends BaseMapper<Admin> {
+}
