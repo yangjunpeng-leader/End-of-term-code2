09ba133d21a02408a31bad17d7763e36b629a0eb
yangjp <1257282045@qq.com>
2023-06-10 20:27:07 +0800

    完善了部分功能

---
+++ src/main/java/com/example/Eight/mapper/AdjustRoomMapper.java
@@ -0,0 +1,7 @@
+package com.example.Eight.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.Eight.entity.AdjustRoom;
+
+public interface AdjustRoomMapper extends BaseMapper<AdjustRoom> {
+}
