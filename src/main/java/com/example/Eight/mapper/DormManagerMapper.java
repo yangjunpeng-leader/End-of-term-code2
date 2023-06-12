c26bf45a17492002606987da0c997d6ce5dbf9ac
republic4 <2449930747@qq.com>
2023-06-12 22:24:44 +0800

    zhangmz提交

---
+++ src/main/java/com/example/Eight/mapper/DormManagerMapper.java
@@ -0,0 +1,8 @@
+package com.example.Eight.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.Eight.entity.DormManager;
+
+
+public interface DormManagerMapper extends BaseMapper<DormManager> {
+}
