e86bd786c37f3cd50275d34f702d74af1ceb7f6a
pixinxin <sedtrfgldfghj@qq.com>
2023-06-12 16:31:32 +0800

    pxx

---
+++ src/main/java/com/example/Eight/mapper/VisitorMapper.java
@@ -0,0 +1,7 @@
+package com.example.Eight.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.Eight.entity.Visitor;
+
+public interface VisitorMapper extends BaseMapper<Visitor> {
+}
