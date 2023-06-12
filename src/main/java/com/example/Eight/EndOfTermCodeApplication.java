97edc17186fa047f2d21d4c198fd49b986c8ff6a
yangjp <1257282045@qq.com>
2023-06-12 22:31:58 +0800

    完善了部分功能

--- src/main/java/com/example/Eight/EndOfTermCodeApplication.java
+++ src/main/java/com/example/Eight/EndOfTermCodeApplication.java
@@ -1,9 +1,11 @@
 package com.example.Eight;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com/example/Eight/mapper")
 public class EndOfTermCodeApplication {
 
     public static void main(String[] args) {
