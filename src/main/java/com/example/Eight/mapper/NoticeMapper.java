245ad5a260269f166a4dbaa7ae2a8a3fcc411d86
yangjp <99852590+yangjunpeng-leader@users.noreply.github.com>
2023-06-12 22:21:35 +0800

    Delete NoticeMapper.java
    删除无法统计代码部分

--- src/main/java/com/example/Eight/mapper/NoticeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.Eight.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.Eight.entity.Notice;
-
-
-public interface NoticeMapper extends BaseMapper<Notice> {
-}
