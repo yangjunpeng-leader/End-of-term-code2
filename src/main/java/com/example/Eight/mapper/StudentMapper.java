40580b0d8d305b4ad33e572f0bd3edee64af1cee
yangjp <99852590+yangjunpeng-leader@users.noreply.github.com>
2023-06-12 22:39:49 +0800

    Delete StudentMapper.java
    删除部分无法统计代码部分

--- src/main/java/com/example/Eight/mapper/StudentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.Eight.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.Eight.entity.Student;
-
-public interface StudentMapper extends BaseMapper<Student> {
-}
