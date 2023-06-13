204171f0d42bc7d11e2677024e8f522b267ad68a
yangjp <99852590+yangjunpeng-leader@users.noreply.github.com>
2023-06-13 10:01:39 +0800

    Delete VisitorMapper.java

--- src/main/java/com/example/Eight/mapper/VisitorMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.Eight.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.Eight.entity.Visitor;
-
-public interface VisitorMapper extends BaseMapper<Visitor> {
-}
